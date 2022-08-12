027f72aea63b8beaa09c11fdb2b10956e6c54942
Surgupta <surgupta@microsoft.com>
2022-08-12 14:42:44 -0700

    Adding StorageConverter class

---
+++ extensions/Worker.Extensions.Storage.Blobs/src/StorageConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.Storage.Blobs
+{
+    internal class StorageConverter
+    {
+    }
+}
