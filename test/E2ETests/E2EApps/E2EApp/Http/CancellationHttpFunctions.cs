7a916b0af125fd8e3a663d6ce793d9f5b883ceed
Lilian Kasem <likasem@microsoft.com>
2022-08-18 14:55:41 -0700

    Add initial e2e tests

---
+++ test/E2ETests/E2EApps/E2EApp/Http/CancellationHttpFunctions.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.Functions.Worker.E2EApp
+{
+    public static class CancellationHttpFunctions
+    {
+        [Function(nameof(HandlesCancellationToken))]
+        public static async Task<HttpResponseData> HandlesCancellationToken(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
+            FunctionContext context,
+            CancellationToken cancellationToken)
+        {
+            var logger = context.GetLogger(nameof(HandlesCancellationToken));
+            logger.LogInformation(".NET Worker HTTP trigger function processed a request");
+
+            try
+            {
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                response.WriteString($"Hello world!");
+
+                await Task.Delay(6000, cancellationToken);
+
+                return response;
+            }
+            catch (OperationCanceledException)
+            {
+                logger.LogInformation("Function invocation cancelled");
+
+                var response = req.CreateResponse(HttpStatusCode.ServiceUnavailable);
+                response.WriteString("Invocation cancelled");
+
+                return response;
+            }
+        }
+
+        [Function(nameof(DoesNotHandleCancellationToken))]
+        public static async Task<HttpResponseData> DoesNotHandleCancellationToken(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
+            FunctionContext context,
+            CancellationToken cancellationToken)
+        {
+            var logger = context.GetLogger(nameof(DoesNotHandleCancellationToken));
+            logger.LogInformation(".NET Worker HTTP trigger function processed a request");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteString($"Hello world!");
+
+            await Task.Delay(6000, cancellationToken);
+
+            return response;
+        }
+    }
+}
---
+++ test/E2ETests/E2ETests/CancellationEndToEndTests.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Azure.Functions.Tests.E2ETests
+{
+    [Collection(Constants.FunctionAppCollectionName)]
+    public class CancellationEndToEndTests
+    {
+        // TODO: add `functionTimeout` to host.json to force timeout
+        private readonly FunctionAppFixture _fixture;
+
+        public CancellationEndToEndTests(FunctionAppFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task HttpTrigger_HandlesCancellationToken_SuccessfullyCancelsInvocation()
+        {
+            var expectedStatusCode = HttpStatusCode.ServiceUnavailable;
+            var expectedMessage = "Invocation cancelled";
+
+            var response = await HttpHelpers.InvokeHttpTrigger("HandlesCancellationToken");
+
+            var actualMessage = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+
+            if (!string.IsNullOrEmpty(expectedMessage))
+            {
+                Assert.False(string.IsNullOrEmpty(actualMessage));
+                Assert.Contains(expectedMessage, actualMessage);
+            }
+        }
+
+        [Fact]
+        public async Task HttpTrigger_DoesNotHandleCancellationToken_Throws_AndThenWhat()
+        {
+            var expectedStatusCode = HttpStatusCode.InternalServerError;
+
+            var response = await HttpHelpers.InvokeHttpTrigger("DoesNotHandleCancellationToken");
+            var actualMessage = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+        }
+    }
+}
